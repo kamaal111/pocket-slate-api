af178f65d8fdd4bbf6f81dbd57fa880d6423427a
Kamaal Farah <kamaal.f1@gmail.com>
2023-07-12 20:26:07 +0000

    Implementing health routers in gin

--- src/routers/routers.go
+++ src/routers/routers.go
@@ -3,12 +3,11 @@ package routers
 import (
 	"fmt"
 	"log"
-	"net/http"
 	"os"
-	"strings"
+
+	"github.com/gin-gonic/gin"
 
 	"github.com/kamaal111/pocket-slate-api/src/health"
-	"github.com/kamaal111/pocket-slate-api/src/translations"
 	"github.com/kamaal111/pocket-slate-api/src/utils"
 )
 
@@ -22,33 +21,38 @@ func Start() {
 		serverAddress = fmt.Sprintf(":%s", port)
 	}
 
-	mux := http.NewServeMux()
+	engine := gin.Default()
+	engine.SetTrustedProxies(nil)
 
-	mux.Handle(
-		"/v1/health/ping",
-		loggerMiddleware(
-			allowHTTPMethods([]string{http.MethodGet})(
-				health.PingHandler)))
-	mux.Handle(
-		strings.Join([]string{"/v1/translations", "supported-locales"}, "/"),
-		loggerMiddleware(
-			allowHTTPMethods([]string{http.MethodGet})(
-				authenticateApps([]string{"pocket-slate"})(
-					translations.GetSupportedLocalesHandler))))
-	mux.Handle(
-		"/v1/translations",
-		loggerMiddleware(
-			allowHTTPMethods([]string{http.MethodPost})(
-				authenticateApps([]string{"pocket-slate"})(
-					translations.MakeTranslationHandler))))
-	mux.Handle(
-		"/",
-		loggerMiddleware(
-			allowHTTPMethods([]string{http.MethodGet})(
-				NotFound)))
+	health.Router(engine)
 
-	log.Printf("Listening on %s...", serverAddress)
+	engine.Run(serverAddress)
 
-	err := http.ListenAndServe(serverAddress, mux)
-	log.Fatal(err)
+	// mux.Handle(
+	// 	"/v1/health/ping",
+	// 	loggerMiddleware(
+	// 		allowHTTPMethods([]string{http.MethodGet})(
+	// 			health.PingHandler)))
+	// mux.Handle(
+	// 	strings.Join([]string{"/v1/translations", "supported-locales"}, "/"),
+	// 	loggerMiddleware(
+	// 		allowHTTPMethods([]string{http.MethodGet})(
+	// 			authenticateApps([]string{"pocket-slate"})(
+	// 				translations.GetSupportedLocalesHandler))))
+	// mux.Handle(
+	// 	"/v1/translations",
+	// 	loggerMiddleware(
+	// 		allowHTTPMethods([]string{http.MethodPost})(
+	// 			authenticateApps([]string{"pocket-slate"})(
+	// 				translations.MakeTranslationHandler))))
+	// mux.Handle(
+	// 	"/",
+	// 	loggerMiddleware(
+	// 		allowHTTPMethods([]string{http.MethodGet})(
+	// 			NotFound)))
+
+	// log.Printf("Listening on %s...", serverAddress)
+
+	// err := http.ListenAndServe(serverAddress, mux)
+	// log.Fatal(err)
 }
