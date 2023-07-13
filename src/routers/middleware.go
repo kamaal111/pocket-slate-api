715064b51566ad287928c50e76f99ddbf2299c5e
Kamaal Farah <kamaal.f1@gmail.com>
2023-07-13 20:24:23 +0000

    Implementing app authentication

--- src/routers/middleware.go
+++ src/routers/middleware.go
@@ -1,51 +1,12 @@
 package routers
 
 import (
-	"encoding/json"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
-	"github.com/kamaal111/pocket-slate-api/src/utils"
 )
 
 func jsonMiddleware() gin.HandlerFunc {
 	return func(context *gin.Context) {
 		context.Writer.Header().Set("Content-Type", "application/json")
 		context.Next()
-	}
-}
-
-func authenticateApps(apps []string) func(next http.HandlerFunc) http.HandlerFunc {
-	return func(next http.HandlerFunc) http.HandlerFunc {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			appName := r.Header.Get("app-name")
-			appVersion := r.Header.Get("app-version")
-			apiKey := r.Header.Get("api-key")
-			if appName == "" || appVersion == "" || apiKey == "" {
-				utils.ErrorHandler(w, "Forbidden", http.StatusForbidden)
-				return
-			}
-
-			rawAppAPIKeys, err := utils.UnwrapEnvironment("APP_API_KEYS")
-			if err != nil {
-				utils.ErrorHandler(w, "Something went wrong", http.StatusInternalServerError)
-				return
-			}
-
-			var appAPIKeys map[string]map[string]string
-			err = json.Unmarshal([]byte(rawAppAPIKeys), &appAPIKeys)
-			if err != nil {
-				utils.ErrorHandler(w, "Something went wrong", http.StatusInternalServerError)
-				return
-			}
-
-			token := appAPIKeys[appName][appVersion]
-			if token == "" || token != apiKey {
-				utils.ErrorHandler(w, "Forbidden", http.StatusForbidden)
-				return
-			}
-
-			next.ServeHTTP(w, r)
-		})
 	}
 }
--- src/translations/router.go
+++ src/translations/router.go
@@ -2,11 +2,15 @@ package translations
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/kamaal111/pocket-slate-api/src/utils"
 )
 
 func Router(engine *gin.Engine) *gin.Engine {
 	prefix := "/v1/translations"
 	group := engine.Group(prefix)
+
+	group.Use(utils.AuthenticateApps([]string{"pocket-slate"}))
+
 	group.POST("", makeTranslationHandler)
 	group.GET("/supported-locales", getSupportedLocalesHandler)
 
